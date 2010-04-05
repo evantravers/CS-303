cf78b46e92e49e4900baef97eaa513e6d2fefce1
Evan Travers <widgetwebworks@gmail.com>
2010-04-05 14:41:50 -0500

    Added the Lab6 folder and files.

---
+++ lab6/cs303/Demo.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.Arrays;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.Vector;
+
+import cs303.Stopwatch;
+import cs303.Data;
+import cs303.Timing;
+import cs303.Graph;
+
+public class Demo {
+    public static void main(String args[]) throws IOException {
+        Data data = new Data();
+        Timing t = new Timing();
+        Graph g = new Graph("graph.off");
+    }
+}
